a33885546db8b5587f9bbf0838cf5d400b1a30b5
Allison <allison@gmail.com>
2024-10-02 11:40:11 -0300

    feat: create enums : Status, UserRole

---
+++ src/main/java/com/br/moviehub/enums/Status.java
@@ -0,0 +1,5 @@
+package com.br.moviehub.enums;
+
+public enum Status {
+    NOT_STARTED, IN_PROGRESS, WATCHED
+}
---
+++ src/main/java/com/br/moviehub/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.br.moviehub.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
