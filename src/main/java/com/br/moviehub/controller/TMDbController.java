2d1dbd6c9c658d209648b69f60f2f1c117d3daac
Allison <allison@gmail.com>
2024-10-02 11:46:20 -0300

    feat: Add findById method to Movie and TvShow in Controller

--- src/main/java/com/br/moviehub/controller/TMDbController.java
+++ src/main/java/com/br/moviehub/controller/TMDbController.java
@@ -1,15 +1,15 @@
 package com.br.moviehub.controller;
 
-import com.br.moviehub.dtos.tmdbDtosApi.MovieResultDto;
-import com.br.moviehub.dtos.tmdbDtosApi.TMDbGenresDto;
-import com.br.moviehub.dtos.tmdbDtosApi.TvResultDto;
+import com.br.moviehub.dtos.TMDB.details.MovieDetailsDto;
+import com.br.moviehub.dtos.TMDB.details.TvShowDetailsDto;
+import com.br.moviehub.dtos.TMDB.filters.MovieResultDto;
+import com.br.moviehub.dtos.TMDB.filters.GenresFilterDto;
+import com.br.moviehub.dtos.TMDB.filters.TvShowResultDto;
 import com.br.moviehub.service.TmdbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,16 +26,25 @@ public class TMDbController {
     }
 
     @GetMapping("/genreMovie")
-    public ResponseEntity<TMDbGenresDto> allGenreMovie() {
+    public ResponseEntity<GenresFilterDto> allGenreMovie() {
         return ResponseEntity.ok(tmdbService.getAllGenresMovie());
     }
 
     @GetMapping("/genreTv")
-    public ResponseEntity<TMDbGenresDto> allGenreTv() {
+    public ResponseEntity<GenresFilterDto> allGenreTv() {
         return ResponseEntity.ok(tmdbService.getAllGenresTv());
     }
 
-    //Movie Methods
+    @GetMapping("/movie/{id}")
+    public ResponseEntity<MovieDetailsDto> getMovieById(@PathVariable Long id) {
+        return ResponseEntity.ok(tmdbService.getMovieById(id));
+    }
+
+    @GetMapping(value = "/tv/{id}")
+    public ResponseEntity<TvShowDetailsDto> getTvById(@PathVariable Long id) {
+        return ResponseEntity.ok(tmdbService.getTvById(id));
+    }
+
     @GetMapping("/discoverMovie")
     public ResponseEntity<MovieResultDto> discoverMovie(@RequestParam(required = false) Boolean adult,
                                                         @RequestParam(required = false) String language,
@@ -76,19 +85,19 @@ public class TMDbController {
 
     //Tv methods
     @GetMapping("/discoverTv")
-    public ResponseEntity<TvResultDto> discoverTv(@RequestParam(required = false) LocalDate air_date_gte,
-                                                  @RequestParam(required = false) Integer first_air_date_year,
-                                                  @RequestParam(required = false) LocalDate first_air_date_gte,
-                                                  @RequestParam(required = false) Boolean include_adult,
-                                                  @RequestParam(required = false) String language,
-                                                  @RequestParam(required = false) Float vote_average_gte,
-                                                  @RequestParam(required = false) Float vote_count_gte,
-                                                  @RequestParam(required = false) String with_genres,
-                                                  @RequestParam(required = false) String with_keywords,
-                                                  @RequestParam(required = false) String with_origin_country,
-                                                  @RequestParam(required = false) String with_original_language,
-                                                  @RequestParam(required = false) String without_genres,
-                                                  @RequestParam(required = false) String without_keywords) {
+    public ResponseEntity<TvShowResultDto> discoverTv(@RequestParam(required = false) LocalDate air_date_gte,
+                                                      @RequestParam(required = false) Integer first_air_date_year,
+                                                      @RequestParam(required = false) LocalDate first_air_date_gte,
+                                                      @RequestParam(required = false) Boolean include_adult,
+                                                      @RequestParam(required = false) String language,
+                                                      @RequestParam(required = false) Float vote_average_gte,
+                                                      @RequestParam(required = false) Float vote_count_gte,
+                                                      @RequestParam(required = false) String with_genres,
+                                                      @RequestParam(required = false) String with_keywords,
+                                                      @RequestParam(required = false) String with_origin_country,
+                                                      @RequestParam(required = false) String with_original_language,
+                                                      @RequestParam(required = false) String without_genres,
+                                                      @RequestParam(required = false) String without_keywords) {
         return ResponseEntity.ok(tmdbService.getDiscoverTv
                 (air_date_gte, first_air_date_year, first_air_date_gte, include_adult, language, vote_average_gte,
                         vote_count_gte, with_genres, with_keywords, with_origin_country, with_original_language,
@@ -96,19 +105,19 @@ public class TMDbController {
     }
 
     @GetMapping("/airingTodayTv")
-    public ResponseEntity<TvResultDto> airingTodayTv(@RequestParam(required = false) String language) {
+    public ResponseEntity<TvShowResultDto> airingTodayTv(@RequestParam(required = false) String language) {
         return ResponseEntity.ok(tmdbService.getAiringTodayTv(language));
     }
     @GetMapping("/popularTv")
-    public ResponseEntity<TvResultDto> popularTv(@RequestParam(required = false) String language) {
+    public ResponseEntity<TvShowResultDto> popularTv(@RequestParam(required = false) String language) {
         return ResponseEntity.ok(tmdbService.getPopularTv(language));
     }
     @GetMapping("/topRatedTv")
-    public ResponseEntity<TvResultDto> topRatedTv(@RequestParam(required = false) String language) {
+    public ResponseEntity<TvShowResultDto> topRatedTv(@RequestParam(required = false) String language) {
         return ResponseEntity.ok(tmdbService.getTopRatedTv(language));
     }
     @GetMapping("/onTheAirTv")
-    public ResponseEntity<TvResultDto> onTheAirTv(@RequestParam(required = false) String language) {
+    public ResponseEntity<TvShowResultDto> onTheAirTv(@RequestParam(required = false) String language) {
         return ResponseEntity.ok(tmdbService.getOnTheAirTv(language));
     }
 }
