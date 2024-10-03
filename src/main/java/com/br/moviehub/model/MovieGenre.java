d11dad16d85824f66a7511bfbb014bed9765694c
Allison <allison@gmail.com>
2024-10-03 17:54:20 -0300

    refactor: rename classes Movie_genre , TvShow_genre, Watchlist_movie, Watchlist_TvShow to MovieGenre, TvShowGenre, WatchlistMovie, WatchlistTvShow.

---
+++ src/main/java/com/br/moviehub/model/MovieGenre.java
@@ -0,0 +1,19 @@
+package com.br.moviehub.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movie_genre")
+public class MovieGenre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+}
