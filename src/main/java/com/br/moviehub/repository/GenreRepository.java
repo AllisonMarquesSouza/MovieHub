e82327cd066fbc8285564ee8fce4a2fe9f4ca378
Allison <allison@gmail.com>
2024-10-21 11:02:53 -0300

    feat(repository): create GenreRepository .

---
+++ src/main/java/com/br/moviehub/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.br.moviehub.repository;
+
+import com.br.moviehub.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
