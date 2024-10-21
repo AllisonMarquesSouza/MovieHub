30a11ae724a06cf78320650e405727fa78db380e
Allison <allison@gmail.com>
2024-10-21 11:15:29 -0300

    feat: create dto for FavoriteMovie : FavoriteMovieDto.

---
+++ src/main/java/com/br/moviehub/dtos/favoriteMovie/FavoriteMovieDto.java
@@ -0,0 +1,12 @@
+package com.br.moviehub.dtos.favoriteMovie;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class FavoriteMovieDto {
+    @NotNull(message = "The userId cannot be null")
+    private Long userId;
+    @NotNull(message = "The movieId cannot be null")
+    private Long movieId;
+}
