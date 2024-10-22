f3f8b8db7bb3e7633170585a1926be96d50f38c7
Allison <allison@gmail.com>
2024-10-22 11:07:30 -0300

    feat(repository): create TvShowRepository .

---
+++ src/main/java/com/br/moviehub/repository/TvShowRepository.java
@@ -0,0 +1,9 @@
+package com.br.moviehub.repository;
+
+import com.br.moviehub.model.TvShow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TvShowRepository extends JpaRepository<TvShow, Long> {
+}
