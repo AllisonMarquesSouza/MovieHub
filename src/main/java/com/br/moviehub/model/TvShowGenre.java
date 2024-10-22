942449770599ec7d5f5ad2298f5feb625affbc81
Allison <allison@gmail.com>
2024-10-22 11:16:17 -0300

    feat(model): Add constructors in the TvShowGenre : @NoArgsConstructor TvShowGenre(TvShow, Genre) .

--- src/main/java/com/br/moviehub/model/TvShowGenre.java
+++ src/main/java/com/br/moviehub/model/TvShowGenre.java
@@ -2,10 +2,12 @@ package com.br.moviehub.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tv_show_genre")
 @Getter
+@NoArgsConstructor
 public class TvShowGenre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +20,9 @@ public class TvShowGenre {
     @ManyToOne
     @JoinColumn(name = "genre_id")
     private Genre genre;
+
+    public TvShowGenre(TvShow tvShow, Genre genre) {
+        this.tvShow = tvShow;
+        this.genre = genre;
+    }
 }
