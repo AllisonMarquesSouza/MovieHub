ef453714428ed672b877281ed3ea167e735fdbdc
Allison <allison@gmail.com>
2024-10-22 11:18:30 -0300

    feat(service): TvShowGenreService add new methods : existsByTvShowIdAndGenreId, save .

--- src/main/java/com/br/moviehub/service/TvShowGenreService.java
+++ src/main/java/com/br/moviehub/service/TvShowGenreService.java
@@ -2,9 +2,11 @@ package com.br.moviehub.service;
 
 import com.br.moviehub.model.Genre;
 import com.br.moviehub.model.TvShow;
+import com.br.moviehub.model.TvShowGenre;
 import com.br.moviehub.repository.TvShowGenreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,5 +25,14 @@ public class TvShowGenreService {
 
     public List<TvShow> findTvShowByGenreName(String genreName) {
         return tvShowGenreRepository.findTvShowByGenreName(genreName);
+    }
+
+    public Boolean existsByTvShowIdAndGenreId(Long tvShowId, Long genreId) {
+        return tvShowGenreRepository.existsByTvShowIdAndGenreId(tvShowId, genreId);
+    }
+
+    @Transactional
+    public TvShowGenre save(TvShowGenre tvShowGenre) {
+        return tvShowGenreRepository.save(tvShowGenre);
     }
 }
