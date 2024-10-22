92b18198d93119daf7ea69edd3ac8a628df1cf3a
Allison <allison@gmail.com>
2024-10-22 10:41:40 -0300

    feat(repository): FavoriteMovieRepository : Changing the method findAllByUser to FindAllByUserId

--- src/main/java/com/br/moviehub/repository/FavoriteMovieRepository.java
+++ src/main/java/com/br/moviehub/repository/FavoriteMovieRepository.java
@@ -1,7 +1,6 @@
 package com.br.moviehub.repository;
 
 import com.br.moviehub.model.FavoriteMovie;
-import com.br.moviehub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface FavoriteMovieRepository extends JpaRepository<FavoriteMovie, Long> {
-    List<FavoriteMovie> findAllByUser(User user);
+    List<FavoriteMovie> findAllByUserId(Long userId);
     Boolean existsByUserIdAndMovieId(Long userId, Long movieId);
     void deleteByUserIdAndMovieId(Long userId, Long movieId);
     void deleteAllByUserId(Long userId);
