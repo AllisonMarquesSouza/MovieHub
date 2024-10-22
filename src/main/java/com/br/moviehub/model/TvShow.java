d405ca69a8e5ded9675d186824811345b305ed2b
Allison <allison@gmail.com>
2024-10-22 11:15:22 -0300

    feat(model): Add constructors in the TvShow : @NoArgsConstructor TvShow(TvShowDetailsDto) && Add @jointable with Genre Table.

--- src/main/java/com/br/moviehub/model/TvShow.java
+++ src/main/java/com/br/moviehub/model/TvShow.java
@@ -1,17 +1,18 @@
 package com.br.moviehub.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.br.moviehub.dtos.TMDB.details.TvShowDetailsDto;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "tv_show")
 @Getter
+@NoArgsConstructor
 public class TvShow {
     @Id
     private Long id;
@@ -24,6 +25,14 @@ public class TvShow {
 
     @Column(name = "overview")
     private String overview;
+
+    @JoinTable(
+            name = "tv_show_genre",
+            joinColumns = @JoinColumn(name = "tv_show_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    @ManyToMany
+    private List<Genre> genres;
 
     @Column(name = "adult")
     private Boolean adult;
@@ -58,4 +67,22 @@ public class TvShow {
     @Column(name = "vote_count")
     private Integer voteCount;
 
+    public TvShow(TvShowDetailsDto tvShowDetails) {
+        this.id = tvShowDetails.getId();
+        this.originalName = tvShowDetails.getOriginal_name();
+        this.originalLanguage = tvShowDetails.getOriginal_language();
+        this.overview = tvShowDetails.getOverview();
+        this.genres = tvShowDetails.getGenres();
+        this.adult = tvShowDetails.getAdult();
+        this.numberOfEpisodes = tvShowDetails.getNumber_of_episodes();
+        this.numberOfSeasons = tvShowDetails.getNumber_of_seasons();
+        this.backdropPath = tvShowDetails.getBackdrop_path();
+        this.firstAirDate = tvShowDetails.getFirst_air_date();
+        this.popularity = tvShowDetails.getPopularity();
+        this.posterPath = tvShowDetails.getPoster_path();
+        this.status = tvShowDetails.getStatus();
+        this.type = tvShowDetails.getType();
+        this.voteAverage = tvShowDetails.getVote_average();
+        this.voteCount = tvShowDetails.getVote_count();
+    }
 }
