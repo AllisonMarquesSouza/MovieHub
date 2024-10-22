50d62c94ba948f543a17293f7b0edde8e2ef82d5
Allison <allison@gmail.com>
2024-10-22 11:24:33 -0300

    feat(controller): Add FavoriteTvShowController for handling methods: getFavoritesByUserId, isTvShowFavorited, addFavorite, deleteFavorite, deleteAllFavorites

---
+++ src/main/java/com/br/moviehub/controller/FavoriteTvShowController.java
@@ -0,0 +1,46 @@
+package com.br.moviehub.controller;
+
+import com.br.moviehub.dtos.favoriteTvShow.FavoriteTvShowDto;
+import com.br.moviehub.model.FavoriteTvShow;
+import com.br.moviehub.service.FavoriteTvShowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("favoriteTvShow")
+@RequiredArgsConstructor
+public class FavoriteTvShowController {
+    private final FavoriteTvShowService favoriteTvShowService;
+
+    @GetMapping("/getByUserId/{id}")
+    public ResponseEntity<List<FavoriteTvShow>> getFavoritesByUserId(@PathVariable Long id){
+        return ResponseEntity.ok(favoriteTvShowService.getFavoritesByUserId(id));
+    }
+
+    @GetMapping("/isFavorited")
+    public ResponseEntity<Boolean> isTvShowFavorited(@RequestBody FavoriteTvShowDto tvShowDto){
+        return ResponseEntity.ok(favoriteTvShowService.isTvShowFavorited(tvShowDto));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<FavoriteTvShow> addFavorite(@RequestBody FavoriteTvShowDto tvShowDto){
+        return new ResponseEntity<>(favoriteTvShowService.addFavorite(tvShowDto), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteFavorite(@RequestBody FavoriteTvShowDto tvShowDto){
+        favoriteTvShowService.deleteFavorite(tvShowDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/deleteAll/{id}")
+    public ResponseEntity<Void> deleteAllFavorites(@PathVariable Long id){
+        favoriteTvShowService.removeAllFavorites(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
