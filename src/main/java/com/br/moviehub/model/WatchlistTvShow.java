5baa4afb7f5440151bd827d3a5a4790f18e9ae7a
Allison <allisonsouza10261@gmail.com>
2024-11-01 09:39:06 -0300

    feat(model): Add constructors in the WatchlistTvShow : @NoArgsConstructor , WatchlistTvShow(User, TvShow)

--- src/main/java/com/br/moviehub/model/WatchlistTvShow.java
+++ src/main/java/com/br/moviehub/model/WatchlistTvShow.java
@@ -3,12 +3,16 @@ package com.br.moviehub.model;
 import com.br.moviehub.enums.Status;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "watchlist_tv_show")
 @Getter
+@Setter
+@NoArgsConstructor
 public class WatchlistTvShow {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +32,11 @@ public class WatchlistTvShow {
     @ManyToOne
     @JoinColumn(name = "tv_show_id")
     private TvShow tvShow;
+
+    public WatchlistTvShow(User user, TvShow tvShowToSave) {
+        this.user = user;
+        this.tvShow = tvShowToSave;
+        this.creationDate = LocalDate.now();
+        this.status = Status.NOT_STARTED;
+    }
 }
