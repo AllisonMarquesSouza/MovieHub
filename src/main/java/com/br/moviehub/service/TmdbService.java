b850f3602c47da1b5184ad5be1f46fc6b35b6ed1
Allison <allisonsouza10261@gmail.com>
2024-11-02 17:04:56 -0300

    fix(TmdbService): Correcting the methods : getDiscoverMovie, getDiscoverTv

--- src/main/java/com/br/moviehub/service/TmdbService.java
+++ src/main/java/com/br/moviehub/service/TmdbService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -90,15 +91,26 @@ public class TmdbService {
                                            String with_keywords, String with_origin_country, String without_genres,
                                            String without_keywords, Integer year) {
 
-        String url3 = String.format(
-                "https://api.themoviedb.org/3/discover/movie?api_key=%s&include_adult=%s&include_video=false&language=%s&page=1&sort_by=popularity.desc" +
-                        "&primary_release_year=%d&region=%s&release_date.gte=%tF&vote_average.gte=%.1f&with_genres=%s" +
-                        "&with_keywords=%s&with_origin_country=%s&without_genres=%s&without_keywords=%s&year=%d",
-                apiKey, adult, language, primary_release_year, region, release_date_gte, vote_average_gte, with_genres,
-                with_keywords, with_origin_country, without_genres, without_keywords, year
-        );
+        String url = UriComponentsBuilder.fromHttpUrl("https://api.themoviedb.org/3/discover/movie")
+                .queryParam("api_key", apiKey)
+                .queryParam("include_adult", adult)
+                .queryParam("include_video", false)
+                .queryParam("language", language)
+                .queryParam("page", 1)
+                .queryParam("sort_by", "popularity.desc")
+                .queryParam("primary_release_year", primary_release_year)
+                .queryParam("region", region)
+                .queryParam("release_date.gte", release_date_gte)
+                .queryParam("vote_average.gte", vote_average_gte)
+                .queryParam("with_genres", with_genres)
+                .queryParam("with_keywords", with_keywords)
+                .queryParam("with_origin_country", with_origin_country)
+                .queryParam("without_genres", without_genres)
+                .queryParam("without_keywords", without_keywords)
+                .queryParam("year", year)
+                .toUriString();
 
-        return doRequest(url3, MovieResultDto.class);
+        return doRequest(url, MovieResultDto.class);
     }
 
     public MovieResultDto getPopularMovies(String language) {
@@ -126,14 +138,25 @@ public class TmdbService {
                                          Boolean include_adult, String language, Float vote_average_gte, Float vote_count_gte,
                                          String with_genres, String with_keywords, String with_origin_country, String with_original_language,
                                          String without_genres, String without_keywords) {
-        String url = String.format(
-                "https://api.themoviedb.org/3/discover/tv?api_key=%s&air_date.gte=%tF&include_adult=%s&language=%s&page=1&sort_by=popularity.desc" +
-                        "&first_air_date_year=%d&first_air_date.gte=%tF&vote_average.gte=%.1f&vote_count.gte=%.1f&with_genres=%s" +
-                        "&with_keywords=%s&with_origin_country=%s&with_original_language=%s&without_genres=%s&without_keywords=%s",
-                apiKey, air_date_gte, include_adult, language, first_air_date_year, first_air_date_gte, vote_average_gte,
-                vote_count_gte, with_genres, with_keywords, with_origin_country, with_original_language, without_genres,
-                without_keywords
-        );
+
+        String url = UriComponentsBuilder.fromHttpUrl("https://api.themoviedb.org/3/discover/tv")
+                .queryParam("api_key", apiKey)
+                .queryParam("air_date.gte", air_date_gte)
+                .queryParam("include_adult", include_adult)
+                .queryParam("language", language)
+                .queryParam("page", 1)
+                .queryParam("sort_by", "popularity.desc")
+                .queryParam("first_air_date_year", first_air_date_year)
+                .queryParam("first_air_date.gte", first_air_date_gte)
+                .queryParam("vote_average.gte", vote_average_gte)
+                .queryParam("vote_count.gte", vote_count_gte)
+                .queryParam("with_genres", with_genres)
+                .queryParam("with_keywords", with_keywords)
+                .queryParam("with_origin_country", with_origin_country)
+                .queryParam("with_original_language", with_original_language)
+                .queryParam("without_genres", without_genres)
+                .queryParam("without_keywords", without_keywords)
+                .toUriString();
 
         return doRequest(url, TvShowResultDto.class);
     }
